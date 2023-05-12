2d080f56f562ffad4b801a4ea5c0cd620250cdd3
AdamStar <adam.starostik@icloud.com>
2023-05-12 13:35:09 +0200

    add RunPython

--- send_email.py
+++ send_email.py
@@ -11,11 +11,13 @@ def send_email(message):
     password = os.getenv(PASSWORD)
     receiver = "adam.starostik9@gmail.com"
     context = ssl.create_default_context()
+    print(password)
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
 
 
-print(PASSWORD)
+
+
 
